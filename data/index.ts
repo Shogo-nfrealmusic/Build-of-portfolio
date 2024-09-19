37f7910c582427e28a3f58d6ee3352cfdaf81888
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-09-19 11:33:59 -0700

    first

---
+++ data/index.ts
@@ -0,0 +1,155 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Approach", link: "#approach" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "My major is law at a Japanese university, but I'm currently studying CS in the United States.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    descriptionClassName: "whitespace-nowrap",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech learner with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Python Django Website",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Are you interested in SHOGO KIKUCHI?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "My First Website",
+    des: "My first WebSite made with HTML, CSS, SCSS, JavaScript. 2024.03",
+    img: "/p1.jpg",
+    iconLists: ["/html.png", "/css.png", "/scss.png", "/js.png"],
+    link: "https://65f6beea0252f167013de6a7--glittery-rolypoly-d3df98.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Work out",
+    des: "I created the website because I like to go to the gym and do strength training. 2024.04",
+    img: "/p2.png",
+    iconLists: ["/html.png", "/css.png", "/scss.png", "/js.png"],
+    link: "https://65fbf5fd8eade19bb765bda7--soft-fudge-62898e.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Coming soon",
+    des: "Coming soon",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "",
+  },
+  {
+    id: 4,
+    title: "Clone Apple Iphone 15pro 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects. 2024.07",
+    img: "/p4.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://shogo-nfrealmusic.github.io/apple_website/",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-app using Python Django",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.png",
+  },
+  {
+    id: 2,
+    title: "CS experience",
+    desc: "Studied in Seattle, Washington State for 1 year as CS major",
+    className: "md:col-span-2", 
+    thumbnail: "/exp2.png",
+  },
+  {
+    id: 3,
+    title: "Japanese university student",
+    desc: "Studied law at a Japanese university, so I have knowledge of law",
+    className: "md:col-span-2", 
+    thumbnail: "/exp3.png",
+  },
+  {
+    id: 4,
+    title: "Volunteer at NPO",
+    desc: "Doing Volunteer with Seattle IT Japanese Professionals, SIJP",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    url: "https://github.com/Shogo-nfrealmusic"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    url: "https://x.com/f__billionaire"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    url: "https://www.linkedin.com/in/shogo-kikuchi-19b792321?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+  },
+];
